b859a0c32e08a1a9388cbeee0bfc356fda2cec08
shijiazhu <shijiazhu@shijiazhus-MacBook-Pro.local>
2018-03-04 23:02:19 -0500

    delete

--- generateRda/heart.R
+++
@@ -1,5 +0,0 @@
-setwd('/Users/shijiazhu/Dropbox/mypaper/GIGSEA/codes/data')
-heartDisease.MetaXcan <- read.table('heart_metaXcan.csv',sep=',',header=T)
-
-setwd('~/Documents/MyPackages/RStudio/GIGSEA/')
-save(heartDisease.MetaXcan,file='data/heartDisease.MetaXcan.rda')
